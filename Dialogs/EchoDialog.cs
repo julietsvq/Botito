1f7b380329d6e9e7f138ddc6a918034ea3d24325
julietsvq <julietsvq@gmail.com>
2018-06-13 15:02:44 +0200

    First Commit

---
+++ Dialogs/EchoDialog.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+
+
+namespace Microsoft.Bot.Sample.SimpleEchoBot
+{
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        protected int count = 1;
+        var message;
+
+        
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            message = context.MakeMessage();
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            if (message.Text == "reset")
+            {
+                PromptDialog.Confirm(
+                    context,
+                    AfterResetAsync,
+                    "Are you sure you want to reset the count?",
+                    "Didn't get that!",
+                    promptStyle: PromptStyle.Auto);
+            }
+            else
+            {
+                var message = context.MakeMessage();
+                message.Speak = SSMLHelper.Speak("Hi, my name is Botito. You said: {message.Text}");
+                await context.PostAsync(message);
+                //await context.PostAsync($"{this.count++}: You said {message.Text}");               
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=barbacodebotito-Web-Deploy.pubxml -p:Password=CEYALxe5u21gcqs6cqwQHmqpJTwjDQTSdDhbyMAFT1tFYp3vn3KBwkFnhodf
+
